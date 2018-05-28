3a87711e43a23cff797f997ffea23be718c95a82
Ru Chern Chong <me@ruchern.com>
2018-05-28 22:42:59 +0800

    Add method to only allow .js files with commands
    Signed-off-by: Ru Chern Chong <me@ruchern.com>

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 client.commands = new Discord.Collection()
 
-const commandFiles = fs.readdirSync(path.join(__dirname, './commands'))
+const commandFiles = fs.readdirSync(path.join(__dirname, './commands')).filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
